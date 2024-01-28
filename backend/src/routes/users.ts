b03be02b94dd2b3cf2884c62291cb6378e8041de
Megh Patel <https://github.com/Megharth/udaan19-website.git>
2024-01-28 13:07:26 +0530

    changes

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,21 @@
 import express,{Request,Response} from 'express';
 import User from '../models/user';
 import jwt from'jsonwebtoken'
+import {check, validationResult} from 'express-validator'
 
 const router = express.Router();
-router.post("/register", async (req:Request,res:Response)=>{
+
+// /api/users/register 
+router.post("/register",[
+    check("firstName","First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email","Email is required").isEmail(),
+    check("password", "Password should be minimum of 6 characters").isLength({min:6,}),
+], async (req:Request,res:Response)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({message:errors.array()})
+    }
     try {
         let user = await User.findOne({
             email:req.body.email,
@@ -13,11 +25,19 @@ router.post("/register", async (req:Request,res:Response)=>{
         }
         user = new User(req.body)
         await user.save();
-        const token = jwt.sign({userId : user.id},process.env.JWT_SECRET_KEY as string);
-
+        const token = jwt.sign({userId : user.id},process.env.JWT_SECRET_KEY as string,{
+            expiresIn:"1d"
+        });
+        res.cookie("auth_token",token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === "production",
+            maxAge:86400000,
+        });
+        return res.sendStatus(200);
     }   
     catch (error){
         console.log(error);
         res.status(500).send({message:"Someting went wrong"});
     }
 })
+export default router;
